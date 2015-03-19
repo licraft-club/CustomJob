a1f22c63d07b7fa8f69cd0af1064f8fd3d5ef11a
unknown <shellljx@gmail.com>
2015-03-19 12:40:09 +0800

    Minecraft服务器转职插件

---
+++ src/com/gmail/shellljx/CustomJob/CustomJob.java
@@ -0,0 +1,20 @@
+package com.gmail.shellljx.CustomJob;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CustomJob extends JavaPlugin {
+
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+		super.onDisable();
+	}
+
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		super.onEnable();
+	}
+
+	
+}
