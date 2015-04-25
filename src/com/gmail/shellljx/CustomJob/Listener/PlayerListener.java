5679b923f86fb5147c458bb8db3eafb946d1da3f
unknown <shellljx@gmail.com>
2015-04-25 21:31:43 +0800

    增加附加功能，禁止两本附魔书在铁毡合成

--- src/com/gmail/shellljx/CustomJob/Listener/PlayerListener.java
+++ src/com/gmail/shellljx/CustomJob/Listener/PlayerListener.java
@@ -1,19 +1,30 @@
 package com.gmail.shellljx.CustomJob.Listener;
 
 import java.awt.Font;
+import java.util.Map;
+import java.util.Set;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.enchantment.EnchantItemEvent;
+import org.bukkit.event.enchantment.PrepareItemEnchantEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerPickupItemEvent;
+import org.bukkit.inventory.AnvilInventory;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryView;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BookMeta;
+import org.bukkit.inventory.meta.EnchantmentStorageMeta;
 
 import com.gmail.shellljx.CustomJob.CustomJob;
 import com.gmail.shellljx.CustomJob.Util.EnglishChatColor;
@@ -26,18 +37,46 @@ public class PlayerListener implements Listener {
 	public PlayerListener(CustomJob plugin){
 		this.plugin = plugin;
 	}
-
+	
 	@EventHandler
 	public void onInteractEvent(InventoryClickEvent e){
 		//System.out.println("值+"+(e.getWhoClicked() instanceof Player)+"--"+e.getCurrentItem().getType()+"--"+e.getCurrentItem().getType().getId());
 		Player player;
+		Inventory inv = e.getInventory();
 		if(e.getWhoClicked() instanceof Player){
-
 			player = (Player) e.getWhoClicked();
 			if(e.getCurrentItem()==null){
 				e.setCancelled(true);
 				return ;
 			}
+			//新增附加功能--判断是否在铁毡下
+			if(inv instanceof AnvilInventory){
+				InventoryView view = e.getView();
+				int rawSlot = e.getRawSlot();
+				if(view.convertSlot(rawSlot)==rawSlot){
+					if(rawSlot==2&&view.getItem(0).getType()==Material.ENCHANTED_BOOK&&view.getItem(1).getType()==Material.ENCHANTED_BOOK){
+						e.setCancelled(true);
+						e.getWhoClicked().closeInventory();
+						((Player)e.getWhoClicked()).sendMessage(ChatColor.RED+"[Customjob]不能相同附魔书附魔");
+//						Map<Enchantment,Integer> map1=((EnchantmentStorageMeta)view.getItem(0).getItemMeta()).getStoredEnchants();
+//						Map<Enchantment,Integer> map2 = ((EnchantmentStorageMeta)view.getItem(1).getItemMeta()).getStoredEnchants();
+//						if(map1.size()==1&&map2.size()==1){
+//							String en1="",en2="";
+//							for(Enchantment enc:map1.keySet())
+//								en1 = enc.getName();
+//							for(Enchantment enc:map2.keySet())
+//								en2 = enc.getName();
+//							System.out.println(en1+"="+en2);
+//							if(en1==en2){
+//								e.setCancelled(true);
+//								e.getWhoClicked().closeInventory();
+//								((Player)e.getWhoClicked()).sendMessage(ChatColor.RED+"[Customjob]不能相同附魔书附魔");
+//							}
+//						}
+					}
+				}
+				}
+			//customjob本来的业务逻辑
 			int id = e.getCurrentItem().getTypeId();
 			if(!plugin.getPlayerConfig().contains(player.getName().toString())){
 				if(plugin.isJobItem(id)&&!player.isOp()){
